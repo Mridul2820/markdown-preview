8279336e852d9ce0ad5ab025a4c615e43b0fc961
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-24 14:08:09 +0530

    markdown creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.scss';
 
 import React from 'react'
 
 const App = () => {
+    const [markdown, setMarkdown] = useState('## Markdown Preview')
+
     return (
-        <div>
-            <h1>Markdown</h1>
-        </div>
+        <main>
+            <section className="markdown">
+                <textarea 
+                    className="input" 
+                    value={markdown} 
+                    onChange={e => setMarkdown(e.target.value)} 
+                    >
+                </textarea>
+                <article className="result">
+                    <ReactMarkdown>
+                        {markdown}
+                    </ReactMarkdown>
+                </article>
+            </section>
+        </main>
     )
 }
 
